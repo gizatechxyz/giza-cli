ec590075b5aad37eb655e133072ccb9cb9aafabf
Gonzalo Mellizo-Soto <gmellizosoto@gmail.com>
2023-07-06 11:19:57 +0200

    Add decorators tests

--- giza/utils/decorators.py
+++ giza/utils/decorators.py
@@ -1,15 +1,21 @@
 from functools import wraps
-from typing import Callable
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from giza.client import ApiClient
 
 
 def auth(func: Callable):
     """
     Check that we have the token and it is not expired before executing
+
+    Expects to be called from an instance of ApiClient to and endpoint that needs authorization
     """
 
     @wraps(func)
-    def wrapper(*args, **kargs):
-        self_ = args[0]
+    def wrapper(*args, **kwargs):
+        self_: ApiClient = args[0]
+
         self_.retrieve_token()
 
         if self_.token is not None:
@@ -17,6 +23,6 @@ def auth(func: Callable):
 
         if self_.token is None or expired:
             raise Exception("Token expired or not set. Log in again.")
-        return func(*args, **kargs)
+        return func(*args, **kwargs)
 
     return wrapper
---
+++ tests/utils/test_decorators.py
@@ -0,0 +1,51 @@
+import os
+from unittest.mock import patch
+
+import pytest
+
+from giza.client import GIZA_TOKEN_VARIABLE, ApiClient
+from giza.utils.decorators import auth
+
+
+def get_client():
+    class DummyClient(ApiClient):
+        @auth
+        def foo(self, *args, **kwargs):
+            return True
+
+    return DummyClient("http://dummy_host")
+
+
+def test_auth_from_env_ok():
+    api_client = get_client()
+
+    with patch.dict(os.environ, {GIZA_TOKEN_VARIABLE: "mytoken"}), patch.object(
+        ApiClient, "_is_expired", return_value=False
+    ) as mock_expired:
+        result = api_client.foo()
+
+    mock_expired.assert_called()
+    assert result is True
+
+
+def test_auth_expired():
+    api_client = get_client()
+
+    with patch.dict(os.environ, {GIZA_TOKEN_VARIABLE: "mytoken"}), patch.object(
+        ApiClient, "_is_expired", return_value=True
+    ) as mock_expired, pytest.raises(Exception):
+        _ = api_client.foo()
+
+    mock_expired.assert_called()
+
+
+def test_auth_no_token():
+    api_client = get_client()
+
+    with patch.dict(os.environ, {GIZA_TOKEN_VARIABLE: "mytoken"}), patch.object(
+        ApiClient,
+        "retrieve_token",
+    ) as mock_retireved, pytest.raises(Exception):
+        _ = api_client.foo()
+
+    mock_retireved.assert_called()
